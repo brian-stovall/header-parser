15a533c677c4f1ec77a1b7e56818341274062ae2
Brian Stovall <hoppenmaus@gmail.com>
2016-02-12 11:47:24 -0500

    added code to parse and display user IP, OS, and preferred language

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ var app = express();
 app.get('*', (request, response) => {
 	//set up response header
 	response.setHeader('Content-Type', 'application/json');
-	response.write(JSON.stringify({'ipaddress'   : null,
-																 'language': null,
-																 'software' : null}),
+
+	//get the ip address, but cut off the port number
+	var ipAddr = request.headers.host;
+	ipAddr = ipAddr.slice(0, ipAddr.indexOf(':'));
+
+	//get the accept-language header and slice off the additional data
+	var primaryLanguage = request.headers['accept-language'];
+	primaryLanguage = primaryLanguage.slice(0, primaryLanguage.indexOf(','));
+
+	//pick the user OS out of the user-agent header info
+	var userOS = request.headers['user-agent'];
+	userOS = userOS.slice(userOS.indexOf('(') + 1, userOS.indexOf(')'));
+
+	//send it as JSON
+	response.write(JSON.stringify({'ipaddress' : ipAddr,
+																 'language'  : primaryLanguage ,
+																 'software'  : userOS }),
 								 (err) => {response.send();});
 });
 
 
 app.listen(process.env.PORT || '8080');
-console.log('timestamp service listening on port 8080');
+console.log('who-am-I service listening on port 8080');
 
